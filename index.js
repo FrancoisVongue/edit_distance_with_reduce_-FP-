7a022d68b5c7eae444392c7f84c6dc43b8cc37c0
FrancoisVongue <48128079+FrancoisVongue@users.noreply.github.com>
2021-07-16 22:31:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
 function editDistance(s1, s2) {
-    
+    s1 = s1.toLowerCase();
+    s2 = s2.toLowerCase();
+
     let table = [...Array(s1.length + 1).keys()]
         .map((rv, ri) => [...Array(s2.length + 1)]
-        .map((v, i) => i + ri));
-    
-    let distanceVector = table.reduce((br, r, ri) => 
-        r.reduce((b, v, i) => {
-            if(ri && i)
-            {
-                const top = br[i];
-                const topleft = br[i-1];
-                const left = b[i-1];
-                
-                let value;
-                
-                if(s1[ri-1] == s2[i-1])
-                    value = Math.min(left, topleft, top);
-                else 
-                    value = Math.min(top + 1, topleft + 1.5, left + 1) // weights for insert(1), change(1.5), delete(1)
-                
-                b[i] = value;
-                return b;
-                
-            } else return b;
-        }, r), table[0]);
-        
-    return distanceVector[s2.length];
+            .map((v, i) => i + ri));
+
+    for(let row = 1; row < table.length; row++) {
+        for(let col = 1; col < table[0].length; col++) {
+            const top = table[row - 1][col];
+            const topleft = table[row - 1][col - 1];
+            const left = table[row][col - 1];
+            const s1Char = s1[row - 1];
+            const s2Char = s2[col - 1];
+            if(s1Char === s2Char) {
+                table[row][col] = topleft;
+            } else {
+                table[row][col] = Math.min(top, topleft, left) + 1;
+            }
+        }
+    }
+
+    return table[s1.length][s2.length];
+};
+
+function relativeEditDistance(search, word) {
+    let distance = editDistance(word, search);
+
+    let distanceReduction = word.length - distance; // how much moved towards a word
+    let searchApplication = 100 * distanceReduction / search.length; // how much you moved, relative to the word length
+    return searchApplication;
 }
-    
-  
